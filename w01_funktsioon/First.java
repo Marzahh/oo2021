7ed9f4aae04b7eaba97fc3b28675b39a8edbed28
Margarita <70900293+Marzahh@users.noreply.github.com>
2021-02-11 21:51:45 +0200

    First homework

---
+++ w01_funktsioon/First.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        int first,second;
+        System.out.print("Enter you first number: ");
+        first = num.nextInt();
+        System.out.print("Enter you second number: ");
+        second = num.nextInt();
+
+
+        if(first>=second){
+           System.out.print("Fist Num is bigger or qual to second number");
+           System.out.print("\n");
+        } else {
+            
+        }
+    }
+}
